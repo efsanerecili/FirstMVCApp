4f452b63ba9bb7605be1eb5c7f8558f223b008b9
efsanerecili <bd8epavpw@dode.edu.az>
2024-05-06 11:53:02 +0400

    first commit

---
+++ WebApplication3/WebApplication3/Controllers/DesignController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Model;
+
+namespace WebApplication3.Controllers
+{
+    public class DesignController : Controller
+    {
+        public IActionResult Index()
+        {
+            Furniture furniture1 = new Furniture()
+            {
+                Id = 1,
+                Name = "Chair", 
+                ImgURL = "img-3.png",
+                Price = 1199.99
+            };
+
+            Furniture furniture2 = new Furniture()
+            {
+                Id = 2,
+                Name = "Sofa",
+                ImgURL = "img-4.png",
+                Price = 1299.99
+            };
+
+            Furniture furniture3 = new Furniture()
+            {
+                Id = 3,
+                Name = "Table",
+                ImgURL = "img-5.png",
+                Price = 1099.99
+            };
+
+            List<Furniture> furnitures = new List<Furniture>();
+
+            furnitures.Add(furniture1);
+            furnitures.Add(furniture2);
+            furnitures.Add(furniture3);
+
+            ViewBag.Furniture = furnitures;
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/WebApplication3/Model/Furniture.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication3.Model
+{
+    public class Furniture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price{ get; set; }
+        public string ImgURL { get; set; }
+
+    }
+}
+
